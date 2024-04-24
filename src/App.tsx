c4210f7c37bb2c0cd878c0608359ed533ffb6b8d
Savecoders <pablopincay2003@gmail.com>
2024-04-24 11:28:53 -0500

    Add: Login  and update SignUp pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@/components/ThemeProvider';
 import { ModeToggle } from './components/ModeToggle';
 import { Routes, Route } from 'react-router-dom';
 import { SignUp } from './pages/SignUp';
+import { Login } from './pages/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<ModeToggle />} />
         <Route path='/SignUp' element={<SignUp />} />
+        <Route path='/Login' element={<Login />} />
       </Routes>
     </ThemeProvider>
   );
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,48 +2,42 @@ import { Button } from '@/components/ui/Button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/Card';
 import { Input } from '@/components/ui/Input';
 import { Label } from '@/components/ui/Label';
+import { Link } from 'react-router-dom';
 
 export function SignUp() {
   return (
-    <Card className='mx-auto max-w-sm'>
-      <CardHeader>
-        <CardTitle className='text-xl'>Sign Up</CardTitle>
-        <CardDescription>Enter your information to create an account</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className='grid gap-4'>
-          <div className='grid grid-cols-2 gap-4'>
-            <div className='grid gap-2'>
-              <Label htmlFor='first-name'>First name</Label>
-              <Input id='first-name' placeholder='Max' required />
+    <section className='w-full h-screen flex items-center justify-center '>
+      <Card className='mx-auto md:min-w-[400px] p-2'>
+        <CardHeader>
+          <CardTitle className='text-3xl'>Sign Up</CardTitle>
+          <CardDescription>Enter your information to create an account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <section className='grid gap-6'>
+            <div className='grid gap-3'>
+              <Label htmlFor='first-name'>Username</Label>
+              <Input id='first-name' placeholder='username' required />
             </div>
-            <div className='grid gap-2'>
-              <Label htmlFor='last-name'>Last name</Label>
-              <Input id='last-name' placeholder='Robinson' required />
+            <div className='grid gap-3'>
+              <Label htmlFor='email'>Email</Label>
+              <Input id='email' type='email' placeholder='exam@example.com' required />
             </div>
+            <div className='grid gap-4'>
+              <Label htmlFor='password'>Password</Label>
+              <Input id='password' type='password' />
+            </div>
+            <Button type='submit' className='w-full'>
+              Create an Account
+            </Button>
+          </section>
+          <div className='mt-4 text-center text-sm'>
+            Already have an Account?{' '}
+            <Link to='#' className='underline'>
+              Sign in
+            </Link>
           </div>
-          <div className='grid gap-2'>
-            <Label htmlFor='email'>Email</Label>
-            <Input id='email' type='email' placeholder='m@example.com' required />
-          </div>
-          <div className='grid gap-2'>
-            <Label htmlFor='password'>Password</Label>
-            <Input id='password' type='password' />
-          </div>
-          <Button type='submit' className='w-full'>
-            Create an account
-          </Button>
-          <Button variant='outline' className='w-full'>
-            Sign up with GitHub
-          </Button>
-        </div>
-        <div className='mt-4 text-center text-sm'>
-          Already have an account?{' '}
-          <a href='#' className='underline'>
-            Sign in
-          </a>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </section>
   );
 }

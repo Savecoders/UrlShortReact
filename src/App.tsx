33b3c377c1ba2d2bbd66b63cc64c58293913ad82
Savecoders <pablopincay2003@gmail.com>
2024-04-23 19:32:14 -0500

    Add: shadcn components and configure theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { ThemeProvider } from '@/components/theme-provider';
+import { ModeToggle } from './components/mode-toggle';
+
 function App() {
   return (
-    <>
-      <p>Hello world!!</p>
-    </>
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      {
+        <>
+          <ModeToggle />
+          <p>Hello word</p>
+        </>
+      }
+    </ThemeProvider>
   );
 }
 

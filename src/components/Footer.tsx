3b4448b2252226d4d0099144c2f52c46842322d8
Pablo Pincay Alvarez <72560306+Savecoders@users.noreply.github.com>
2024-10-03 16:26:36 +0000

    add: refactor footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
   return (
-    <footer className='px-10 py-5 my-24 relative max-w-5xl mx-auto rounded-md border-[0.2px] border-teal-800 text-center'>
-      UrlTiny
+    <footer className='px-10 py-2 my-24 relative flex justify-between max-w-5xl mx-auto rounded-md  border-teal-800 text-center'>
+      <div className='flex flex-col text-left gap-3'>
+        <p>© 2024 - UrlTiny. All rights reserved.</p>
+        <p>
+          Created by{' '}
+          <a className='text-teal-300' href='https://github.com/Savecoders'>
+            Savecoders
+          </a>
+        </p>
+      </div>
+      <Link to='' className='text-muted-foreground hover:text-foreground'>
+        Privaticy Police
+      </Link>
     </footer>
   );
 }
